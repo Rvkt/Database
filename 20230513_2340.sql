ba87576e1001d773933fc5b3d83bbbab5f49ff92
Ravi Kant <iRvhny@gmail.com>
2023-05-14 10:34:27 +0530

    202305141034_MySql

---
+++ 20230513_2340.sql
@@ -0,0 +1,16 @@
+-- mysql is not case-sensitive
+ 
+-- create databse named myDB
+-- end your statement with semicolon;
+-- CREATE DATABASE myDB; 
+
+-- to use the created database
+USE mydb;
+
+-- to delte the database
+DROP DATABASE mydb;
+
+-- to make the database read only
+-- ALTER DATABASE mydb READ ONLY = 1;
+
+
